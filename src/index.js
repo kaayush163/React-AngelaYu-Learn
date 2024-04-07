d5b90c7a836cde31a0d6c62cf6bd9a499b442b9b
Aayush . <88710500+kaayush163@users.noreply.github.com>
2024-04-07 12:55:11 +0000

    image insert

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const food1 = "Bacon";
+const food2 = "Noodles";
+const food3 = "Jamon";
+const name = "Aayush";
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <h1 className="heading">{name} Favourite Foods</h1>
     <ul>
-      <li>Bacon</li>
-      <li>Noodles</li>
-      <li>Jamon</li>
+      <li> {food1} </li>
+      <img
+        className="food-img"
+        alt="bacon"
+        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*"
+      />
+      <img
+        className="food-img"
+        alt="jamon"
+        src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"
+      />
+      <img
+        className="food-img"
+        alt="noodles"
+        src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"
+      />
+      <li> {food2} </li>
+      <li> {food3} </li>
     </ul>
   </div>,
   document.getElementById("root"),
