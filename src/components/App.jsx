45170c9122651395a58f419d7e66f7b214cef3e1
Aayush . <88710500+kaayush163@users.noreply.github.com>
2024-04-13 12:31:38 +0000

    Task to deal with props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,36 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">Mycontacts</h1>
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].img}
+        tel={contacts[0].tel}
+        email={contacts[0].email}
+      />
+      {/* <div className="card">
+        <div className="top">
+          <h2 className="name">Alisha</h2>
+          <img
+            className="circle-img"
+            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+            alt="avatar_img"
+          />
+        </div>
+        <div className="bottom">
+          <p className="info">{props.tel}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
 // import "./styles.css";
 
 // export default function App() {
@@ -9,24 +42,24 @@
 //   );
 // }
 
-import React from "react";
-import Heading from "./Heading";
-import List from "./List";
-// import PI, { doublePi, triplePi } from "./math";
-import * as pi from "./math";
-console.log(pi);
-function App() {
-  return (
-    <div>
-      <Heading />
-      <List />
-      <List />
-      <li>{pi.default}</li>
-      <li>{pi.doublePi()}</li>
-      <li>{pi.triplePi()}</li>
-      <li>{pi.add(2, 3)}</li>;
-    </div>
-  );
-}
+// import React from "react";
+// import Heading from "./Heading";
+// import List from "./List";
+// // import PI, { doublePi, triplePi } from "./math";
+// import * as pi from "./math";
+// console.log(pi);
+// function App() {
+//   return (
+//     <div>
+//       <Heading />
+//       <List />
+//       <List />
+//       <li>{pi.default}</li>
+//       <li>{pi.doublePi()}</li>
+//       <li>{pi.triplePi()}</li>
+//       <li>{pi.add(2, 3)}</li>;
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
---
+++ src/contacts.js
@@ -0,0 +1,10 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    img: "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    tel: "+123 456 789",
+    email: "Beynce.com",
+  },
+];
+
+export default contacts;
