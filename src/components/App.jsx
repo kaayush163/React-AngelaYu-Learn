c00cee5fb3dc5b6973370f2929a35424e84423a4
Aayush . <88710500+kaayush163@users.noreply.github.com>
2024-04-14 15:08:40 +0000

    emojipedia to revise map concept

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Card from "./Card";
-import contacts from "../contacts";
-import Avatar from "./AVatar";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
 
-function createCard(contact) {
+console.log(emojipedia);
+
+function createEntry(emojiTerm) {
   return (
-    <Card
-      key={contact.id}
-      name={contact.name}
-      img={contact.img}
-      tel={contact.phone}
-      email={contact.email}
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
     />
   );
 }
@@ -18,34 +18,65 @@ function createCard(contact) {
 function App() {
   return (
     <div>
-      <h1 className="heading">Mycontacts</h1>
-      <Avatar img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" />
-      {contacts.map(createCard)}
-      {/* <Card
-        name={contacts[0].name}
-        img={contacts[0].img}
-        tel={contacts[0].tel}
-        email={contacts[0].email}
-      /> */}
-      {/* <div className="card">
-        <div className="top">
-          <h2 className="name">Alisha</h2>
-          <img
-            className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">{props.tel}</p>
-          <p className="info">{props.email}</p>
-        </div>
-      </div> */}
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
     </div>
   );
 }
 
 export default App;
+
+// import React from "react";
+// import Card from "./Card";
+// import contacts from "../contacts";
+// import Avatar from "./AVatar";
+
+// function createCard(contact) {
+//   return (
+//     <Card
+//       key={contact.id}
+//       name={contact.name}
+//       img={contact.img}
+//       tel={contact.phone}
+//       email={contact.email}
+//     />
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <h1 className="heading">Mycontacts</h1>
+//       <Avatar img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" />
+//       {contacts.map(createCard)}
+//       {/* <Card
+//         name={contacts[0].name}
+//         img={contacts[0].img}
+//         tel={contacts[0].tel}
+//         email={contacts[0].email}
+//       /> */}
+//       {/* <div className="card">
+//         <div className="top">
+//           <h2 className="name">Alisha</h2>
+//           <img
+//             className="circle-img"
+//             src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+//             alt="avatar_img"
+//           />
+//         </div>
+//         <div className="bottom">
+//           <p className="info">{props.tel}</p>
+//           <p className="info">{props.email}</p>
+//         </div>
+//       </div> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
 // import "./styles.css";
 
 // export default function App() {
