77cd5fc5224c8e8137f06004a74a8a9ab2347a51
Aayush . <88710500+kaayush163@users.noreply.github.com>
2024-04-14 14:53:02 +0000

    map to reduce repititive card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,30 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./AVatar";
 
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.img}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">Mycontacts</h1>
       <Avatar img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" />
-
-      <Card
+      {contacts.map(createCard)}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].img}
         tel={contacts[0].tel}
         email={contacts[0].email}
-      />
+      /> */}
       {/* <div className="card">
         <div className="top">
           <h2 className="name">Alisha</h2>
