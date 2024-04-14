c81c09815a82d8fe3c4f0aa61225fa408eb326fa
Aayush . <88710500+kaayush163@users.noreply.github.com>
2024-04-14 17:23:07 +0000

    map,reduce,filter,find,findIndex,forEach learned

---
+++ src/mapfilter.js
@@ -0,0 +1,62 @@
+var numbers = [3, 56, 2, 48, 5];
+
+function double(x) {
+  return x * 2;
+}
+
+const newarray = numbers.map(double);
+
+var newNumbers = [];
+
+function double(x) {
+  newNumbers.push(x * 2);
+}
+
+numbers.forEach(double);
+
+//or
+
+numbers.forEach(function double(x) {
+  newNumbers.push(x * 2);
+});
+
+//Using map is very much concise than foreach because it return overaall a new array
+
+const newmapNumbers = numbers.map(function (x) {
+  return x * 2;
+});
+
+console.log(newmapNumbers);
+
+var filterNumber = [3, 56, 48, 2, 5];
+const newfilter = numbers.filter(function (num) {
+  return num > 10;
+});
+cosnole.log(newfilter);
+
+numbers.forEach(function (num) {
+  if (num < 10) {
+    newNumbers.push(num);
+  }
+});
+
+var reduceNumber = [3, 56, 2, 48, 5];
+
+var count = 0;
+
+reduceNumber.forEach(function (curr) {
+  count += curr;
+});
+
+var newreduce = reduceNumber.reduce(function (accumulator, curr) {
+  return accumulator + curr;
+});
+
+const findnum = numbers.find(function (i) {
+  return i > 10;
+});
+
+const findIndex = numbers.findIndex(function (num) {
+  return num > 10;
+});
+//return index of elemnet which value is greated thean 10
