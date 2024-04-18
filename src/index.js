900ee968ea4bbbb7c1dcb2711421e86bd3d15fbe
Aayush . <88710500+kaayush163@users.noreply.github.com>
2024-04-18 17:57:29 +0000

    Task of Destructuring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import animals, { useAnimal } from "./data";
+import cars from "./practice";
+console.log(cars);
+const [honda, tesla] = cars;
+console.log(honda);
+console.log(tesla);
 
-const [cat, dog] = animals;
-console.log(useAnimal(cat));
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+} = tesla;
 
-const [animal, makeSound] = useAnimal(cat);
-console.log(animal);
-makeSound();
+const {
+  coloursByPopularity: [hondaTopColour],
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour],
+} = tesla;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root"),
+);
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+// import animals, { useAnimal } from "./data";
+
+// const [cat, dog] = animals;
+// console.log(useAnimal(cat));
+
+// const [animal, makeSound] = useAnimal(cat);
+// console.log(animal);
+// makeSound();
 
 // import React from "react";
 // import ReactDOM from "react-dom";
