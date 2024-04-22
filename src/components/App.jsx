70fde3d515e458627c09de7e46fe5d937e8dbe44
Aayush . <88710500+kaayush163@users.noreply.github.com>
2024-04-22 18:43:10 +0000

    To-Do task adding in list form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,98 @@
 import React, { useState } from "react";
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: "",
-  });
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
-    const { name, value } = event.target;
-    // setContact((prevValue) => ({ ...prevValue, [name]: value }));
-    setContact((prevValue) => {
-      return {
-        ...prevValue,
-        [name]: value,
-        // event.target.name:value can't use event inside set state otherwise warnings will be shown
-      };
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
     });
+    setInputText("");
   }
 
   return (
     <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input
-          onChange={handleChange}
-          name="fName"
-          value={contact.fName}
-          placeholder="First Name"
-        />
-        <input
-          onChange={handleChange}
-          name="lName"
-          value={contact.lName}
-          placeholder="Last Name"
-        />
-        <input
-          onChange={handleChange}
-          name="email"
-          value={contact.email}
-          placeholder="Email"
-        />
-        <button>Submit</button>
-      </form>
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [contact, setContact] = useState({
+//     fName: "",
+//     lName: "",
+//     email: "",
+//   });
+
+//   function handleChange(event) {
+//     const { name, value } = event.target;
+//     // setContact((prevValue) => ({ ...prevValue, [name]: value }));
+//     setContact((prevValue) => {
+//       return {
+//         ...prevValue,
+//         [name]: value,
+//         // event.target.name:value can't use event inside set state otherwise warnings will be shown
+//       };
+//     });
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1>
+//         Hello {contact.fName} {contact.lName}
+//       </h1>
+//       <p>{contact.email}</p>
+//       <form>
+//         <input
+//           onChange={handleChange}
+//           name="fName"
+//           value={contact.fName}
+//           placeholder="First Name"
+//         />
+//         <input
+//           onChange={handleChange}
+//           name="lName"
+//           value={contact.lName}
+//           placeholder="Last Name"
+//         />
+//         <input
+//           onChange={handleChange}
+//           name="email"
+//           value={contact.email}
+//           placeholder="Email"
+//         />
+//         <button>Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // import React, { useState } from "react";
 
